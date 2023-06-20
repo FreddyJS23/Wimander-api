0340783de7fe08b89d899b0a8ed312805c8244bb
Freddy Solarte <freddyjsolarte@gmail.com>
2023-06-20 18:16:43 -0400

    relaciones en modelo cliente

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Customer extends Model
 {
@@ -14,4 +15,15 @@ class Customer extends Model
         'mac',
         'locked',
     ];
+
+/**
+ * Get the connection associated with the Customer
+ *
+ * @return \Illuminate\Database\Eloquent\Relations\HasOne
+ */
+public function connection(): HasOne
+{
+    return $this->hasOne(Connection::class);
+}
+
 }
