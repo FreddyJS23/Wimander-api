96d90fc93ffece5897fd4c22deefc694985c41a7
Freddy Solarte <freddyjsolarte@gmail.com>
2023-06-20 18:11:54 -0400

    Relaciones en modelo usuario

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -25,6 +26,7 @@ class User extends Authenticatable
         'active',
     ];
 
+
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -44,4 +46,26 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+/**
+ * Get the connection associated with the User
+ *
+ * @return \Illuminate\Database\Eloquent\Relations\HasOne
+ */
+public function connection(): HasOne
+{
+    return $this->hasOne(Connection::class);
+}
+
+/**
+ * Get the customer associated with the User
+ *
+ * @return \Illuminate\Database\Eloquent\Relations\HasOne
+ */
+public function customer(): HasOne
+{
+    return $this->hasOne(Customer::class);
+}
+
+
 }
