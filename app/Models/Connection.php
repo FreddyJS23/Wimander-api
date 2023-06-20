0066a270bb02b7eebce4d6f59fd9892d889fb543
Freddy Solarte <freddyjsolarte@gmail.com>
2023-06-20 15:03:25 -0400

    relleno de modelo conexion

--- app/Models/Connection.php
+++ app/Models/Connection.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Connection extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'start_date',
+        'expiration_date',
+        'amount',
+    ];
 }
