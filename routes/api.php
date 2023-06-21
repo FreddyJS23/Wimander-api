a89a3eb4b8bb98b204d2b1ff48361b9d68c15bba
Freddy Solarte <freddyjsolarte@gmail.com>
2023-06-20 20:25:13 -0400

    rutas preliminares

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +20,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::controller(UserController::class)->group(function(){
+    Route::get('/user','index')->name('index');
+    Route::post('/user/store','store')->name('store');
+    Route::get('/user/{id}','show')->name('show');
+    Route::put('/user{id}','update')->name('update');
+    Route::delete('/user{id}','delete')->name('delete');
+    
+});
 
- 
+Route::controller(CustomerController::class)->group(function(){
+    Route::get('/customer','index')->name('index');
+    Route::post('/customer/store','store')->name('store');
+    Route::post('/customer/{id}/extends-date','store')->name('extend date');
+    Route::get('/customer/{id}','show')->name('show');
+    Route::put('/customer{id}','update')->name('update');
+    Route::delete('/customer{id}','delete')->name('delete');
+    
+});
