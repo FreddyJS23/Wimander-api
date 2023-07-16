67a1d0316e6f414588707c66fd464a4bd50a7991
Freddy Solarte <freddyjsolarte@gmail.com>
2023-07-16 15:08:53 -0400

    refactor: rutas

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,6 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ConnectionController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\UserController;
-use App\Models\Connection;
-use App\Models\Customer;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,60 +17,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//ruta login
+Route::post('/login', [AuthController::class, 'loginUser']);
+Route::post('/register', [AuthController::class, 'createUser']);
 
 
-Route::post('/login',[AuthController::class,'loginUser'] );
-Route::post('/register',[AuthController::class,'createUser'] );
+Route::middleware('auth:sanctum')->group(function () {
 
+    //clientes
+    Route::apiResource('customer', CustomerController::class);
 
-
-   
-Route::middleware('auth:sanctum')->group(function (){
-
-       
-   
-   
-        Route::controller(CustomerController::class)->group(function(){
-        Route::get('/customer','index')->name('index');
-        Route::post('/customer/store','store')->name('store');
-        Route::get('/customer/{id}','show')->name('show');
-        Route::put('/customer/{id}','update')->name('update');
-        Route::delete('/customer/{id}','destroy')->name('destroy');
-        
-    });
-     
+    //actulizar fecha vencimineto de la conexion
     Route::put('/customer/extends-date/{id}', ConnectionController::class)->name('extend date');
 
+    //datos usuarios
+    Route::resource('user', UserController::class)->only(['show', 'update']);
 
-    Route::controller(UserController::class)->group(function(){
-        Route::get('/user','index')->name('index');
-        Route::post('/user/store','store')->name('store')->withoutMiddleware('auth:sanctum');
-        Route::get('/user/{id}','show')->name('show');
-        Route::put('/user/{id}','update')->name('update');
-        Route::delete('/user/{id}','destroy')->name('destroy');
-        
-    });
-    
-
-
+    //acciones para usuario que solo hacen admins
+    Route::resource('user', UserController::class)->only(['index', 'destroy']);
 });
 
-//responder rutas no autorizadas
-Route::any('/error', function () {
-    return response()->json(['message'=>'Proporcione un token de autorizacion'],401);
+//responder rutas no autorizadas en caso que no tenga el accept "application/json"
+Route::get('/error', function () {
+    return response()->json(['message' => 'Unauthenticated.'], 401);
 })->name('login');
 
-//responder rutas endpoint inexistentes
+//responder rutas endpoint inexistentes en caso que no tenga el accept "application/json"
 Route::fallback(function () {
     return response()->json([
         'message' => 'El endpoint solicitado no existe'
-    ], 404);});
-
-
-
-
-
-
-
-
-    
+    ], 404);
+});
