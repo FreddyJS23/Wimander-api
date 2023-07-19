0aa14bc1b041abe00ab6ecf19d18ea0c15b38588
Freddy Solarte <freddyjsolarte@gmail.com>
2023-07-19 16:39:25 -0400

    refactor: change modelo connection
    se agrego la busqueda implicita del id usando el modelo en los parametros de las funciones

--- app/Http/Controllers/ConnectionController.php
+++ app/Http/Controllers/ConnectionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ConnectionRequest;
+use App\Models\Connection;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
@@ -14,21 +15,19 @@ class ConnectionController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(ConnectionRequest $request, $id)
+    public function __invoke(ConnectionRequest $request,Connection $customerID )
     {
-        $customerConnection = Customer::find($id)->connection;
-
-        $fechaVencimiento = new DateTime($customerConnection->expiration_date);
+        $fechaVencimiento = new DateTime($customerID->expiration_date);
 
         if ($request->extendsDate == "15D") {
 
-            $customerConnection->connection->expiration_date = $fechaVencimiento->add(new DateInterval('P15D'))->format('Y-m-d');
-            $customerConnection->save();
+            $customerID->expiration_date = $fechaVencimiento->add(new DateInterval('P15D'))->format('Y-m-d');
+            $customerID->save();
        
         } elseif ($request->extendsDate == "30D") {
 
-            $customerConnection->expiration_date = $fechaVencimiento->add(new DateInterval('P30D'))->format('Y-m-d');
-            $customerConnection->save();
+            $customerID->expiration_date = $fechaVencimiento->add(new DateInterval('P30D'))->format('Y-m-d');
+            $customerID->save();
         }
 
         return  response()->json(['status' => true], 200);
