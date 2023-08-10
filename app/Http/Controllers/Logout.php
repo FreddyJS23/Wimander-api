8c3b2906b8a67c52b5aece11eb4eff307f0b708a
Freddy Solarte <freddyjsolarte@gmail.com>
2023-08-10 14:32:54 -0400

    feat: change logout sanctum
    como se usa la session del token crfs en la spa, no se esta brindando el token api, por ende ahora se hara  un deslogeo, se invalidaria la session creada por el crfs y regenerara el token

--- app/Http/Controllers/Logout.php
+++ app/Http/Controllers/Logout.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class logout extends Controller
 {
@@ -11,7 +12,14 @@ class logout extends Controller
      */
     public function __invoke(Request $request)
     {
-       $request->user()->currentAccessToken()->delete();
-       return response()->json(['status'=>true],200);
+        //$request->user()->currentAccessToken()->delete();
+        
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->json(['status' => true], 200);
     }
 }
